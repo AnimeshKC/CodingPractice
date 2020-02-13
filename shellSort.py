59953d73a82773fdfc308bf6f3c3ca9fe206fa32
AnimeshKC <animesh@ualberta.ca>
2020-02-13 15:51:52 -0700

    fixed variable name error

--- shellSort.py
+++ shellSort.py
@@ -1,5 +1,4 @@
 import random
-#Identify and resolve sorting error
 def shellSort(arr):
     arrSize = len(arr)
     #Using Knuth's increments  1, 4, 13, 40, ...
@@ -9,21 +8,17 @@ def shellSort(arr):
     while increment >= 1:
         for i in range(increment, arrSize):
             j = i
-            newIndex = j - increment
-            newValue = arr[newIndex]
-            currentValue = arr[j]
-            while j >= increment and currentValue < newValue:
-                swapIndex = j - increment
-                arr[j], arr[swapIndex] = arr[swapIndex], arr[j]
+            while j >= increment and arr[j] < arr[j - increment]:
+                arr[j], arr[j - increment] = arr[j - increment], arr[j]
                 j = j - increment
         increment = increment // 3
 
 def main():
     minInt = 0
     maxInt = 255
-    numRange = 50
-    randomList = [random.randrange(minInt, maxInt) for i in range(numRange)]
-    #randomList = [7, 11, 89, 2, 56, 12, 8, 10, 0, 55, 23]
+    numRange = 14
+    #randomList = [random.randrange(minInt, maxInt) for i in range(numRange)]
+    randomList = [111, 165, 149, 254, 232, 100, 181, 118, 250, 229, 88, 208, 109, 254, 17]
     print("Before the sort: ")
     print(randomList)
     print("\n After the sort: ")
