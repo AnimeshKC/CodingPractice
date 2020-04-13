fcda5046859067da24408b7f1a34907068750d55
AnimeshKC <animesh@ualberta.ca>
2020-04-13 16:20:24 -0600

    c# gradebooks modularization

---
+++ c#Practice/GradeBooks/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GradeBooks
+{
+    class Book
+    {
+        double average;
+        double highestGrade;
+        List<double> grades;
+        string name;
+        public Book(string name)
+        {
+            this.name = name;
+            this.average = 0;
+            this.highestGrade = double.MinValue;
+            this.grades = new List<double>() { };
+
+        }
+        public void addGrade(double grade) //adds a grade and also updates the average
+        {
+            grades.Add(grade);
+            average += (grade - average)/ grades.Count; //constant time update that reduces chance of overflowing
+            updateHighest(grade);
+        }
+        private void updateHighest(double grade)
+        {
+            if (grade > highestGrade)
+            {
+                highestGrade = grade;
+            }
+        }
+        public void statistics()
+        {
+            Console.WriteLine($"In this gradebook, {name}, \n The average grade is {average} \n  The highest grade is {highestGrade}");
+        }
+    }
+
+}
