d803399e2c7f4b1ddb6ceae0fa3cda98ec07c1fc
AnimeshKC <animesh@ualberta.ca>
2020-04-24 15:52:17 -0600

    cleaning up the code and adding documentation

--- PythonPractice/maxNonAdjacent.py
+++ PythonPractice/maxNonAdjacent.py
@@ -1,17 +1,24 @@
 
 def maxNonAdjacent(arr):
+    '''returns the maximum sum of non-adjacent numbers using dynamic programming
+    Time Complexity: O(N)
+    Space Complexity: O(1)'''
+    #[30, 20, 10, 5]
     exclusiveMax = 0 #represents the maximum sum before the current number
     inclusiveMax = 0 #represents the maximum sum including the current number
 
     for num in arr:
-        previousMax = max(exclusiveMax, inclusiveMax) 
+        previousMax = inclusiveMax #get maximum before this element
         #update the inclusive and exclusive max
         inclusiveMax = max(previousMax, exclusiveMax + num) 
         exclusiveMax = previousMax #update exclusive max to be the previous maximum
     return inclusiveMax
 
 if __name__ == "__main__":
-    assert(maxNonAdjacent([30, 20, 10, 5]) == 40)
-    assert(maxNonAdjacent([4, 1, 1, 4, 2, 1]) == 9)
-    print("Tests have passed")
+    assert(maxNonAdjacent([30, 20, 10, 5]) == 40) #simple test 
+    assert(maxNonAdjacent([4, 1, 1, 4, 2, 1]) == 9) #test for not necessarily 2 apart
+    assert(maxNonAdjacent([]) == 0) #test for empty array
+    assert(maxNonAdjacent([15]) == 15) #test for array with one element
+    assert(maxNonAdjacent([1, 2, 1, 6]) == 8) #test for not using the first element
+    print("All Tests have passed")
 
