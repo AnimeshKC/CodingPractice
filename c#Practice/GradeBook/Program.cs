197063308b03372e5fbb4a44b0efd442ccd22d79
AnimeshKC <animesh@ualberta.ca>
2020-04-24 21:12:53 -0600

    Tests

---
+++ c#Practice/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book zenBook = new Book("Zen");
+
+            foreach (double grade in new List<double> { 76, 82, 58, 44 })
+            {
+                zenBook.addGrade(grade);
+            }
+            zenBook.statistics();
+            zenBook.addGrade(89);
+            zenBook.statistics();
+        }
+    }
+}
