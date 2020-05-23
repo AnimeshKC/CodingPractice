169b21fdd257f81c199bb3437f377ce4beba8cd2
AnimeshKC <animesh@ualberta.ca>
2020-05-22 23:35:37 -0600

    fix up linked list

--- DataStructures/singlyLinkedList.py
+++ DataStructures/singlyLinkedList.py
@@ -1,282 +1,104 @@
 
-class Node:
-    # default value of data and link is none if no data is passed
-    def __init__(self, data=None, link=None):
+class ListNode:
+    """
+    A node in a singly-linked list.
+    """
+
+    def __init__(self, data=None, next=None):
         self.data = data
-        self.next = link
+        self.next = next
 
-    # method to update the data feild of Node
-    def updateData(self, data):
-        self.data = data
-
-    # method to set Link feild the Node
-    def setNext(self, node):
-        self.next = node
-
-    # method returns data feild of the Node
-    def getData(self):
-        return self.data
-
-    # method returns address of the next Node
-    def getNextNode(self):
-        return self.next
+    def __repr__(self):
+        return repr(self.data)
 
 
-class LinkedList:
+class SinglyLinkedList:
     def __init__(self):
+        """
+        Create a new singly-linked list.
+        Takes O(1) time.
+        """
         self.head = None
 
-    def addToFront(self, data):
-        frontNode = Node(data)
-        frontNode.setNext(self.head)
-        self.head = frontNode
+    def __repr__(self):
+        """
+        Return a string representation of the list.
+        Takes O(n) time.
+        """
+        nodes = []
+        curr = self.head
+        while curr:
+            nodes.append(repr(curr))
+            curr = curr.next
+        return '[' + ', '.join(nodes) + ']'
 
-    def addToBack(self, data):
-        start = self.head
-        backNode = Node(data)
-        while start.getNextNode():
-            start = start.getNextNode()
-        start.setNext(backNode)
+    def prepend(self, data):
+        """
+        Insert a new element at the beginning of the list.
+        Takes O(1) time.
+        """
+        self.head = ListNode(data=data, next=self.head)
 
-    # method adds elements to the right of the Linked List
-    def addToEnd(self, data):
-        start = self.head
-        tempNode = Node(data)
-        while start.getNextNode():
-            start = start.getNextNode()
-        start.setNext(tempNode)
-        del tempNode
-        # return True
-
-    # method displays Linked List
-    def display(self):
-        start = self.head
-        if start is None:
-            print("Empty List!!!")
+    def append(self, data):
+        """
+        Insert a new element at the end of the list.
+        Takes O(n) time.
+        """
+        if not self.head:
+            self.head = ListNode(data=data)
             return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = ListNode(data=data)
 
-        while start:
-            print(str(start.getData()), end=" ")
-            start = start.next
-            if start:
-                print("-->", end=" ")
-        print()
+    def find(self, key):
+        """
+        Search for the first element with `data` matching
+        `key`. Return the element or `None` if not found.
+        Takes O(n) time.
+        """
+        curr = self.head
+        while curr and curr.data != key:
+            curr = curr.next
+        return curr  # Will be None if not found
 
-    # method returns length of linked list
-    def length(self):
-        start = self.head
-        size = 0
-        while start:
-            size += 1
-            start = start.getNextNode()
-        # print(size)
-        return size
+    def remove(self, key):
+        """
+        Remove the first occurrence of `key` in the list.
+        Takes O(n) time.
+        """
+        # Find the element and keep a
+        # reference to the element preceding it
+        curr = self.head
+        prev = None
+        while curr and curr.data != key:
+            prev = curr
+            curr = curr.next
+        # Unlink it from the list
+        if prev is None:
+            self.head = curr.next
+        elif curr:
+            prev.next = curr.next
+            curr.next = None
 
-    # method returns index of the recieved data
-    def index(self, data):
-        start = self.head
-        position = 1
-
-        while start:
-            if start.getData() == data:
-                return position
-            else:
-                position += 1
-                start = start.getNextNode()
-
-    # method removes item passed from the Linked List
-
-    def remove(self, item):
-        start = self.head
-        previous = None
-        found = False
-
-        # search element in list
-        while not found:
-            if start.getData() == item:
-                found = True
-            else:
-                previous = start
-                start = start.getNextNode()
-
-        # if previous is None then the data is found at first position
-        if previous is None:
-            self.head = start.getNextNode()
-        else:
-            previous.setNext(start.getNextNode())
-        return found
-
-    # method returns max element from the List
-    def Max(self):
-        start = self.head
-        largest = start.getData()
-        while start:
-            if largest < start.getData():
-                largest = start.getData()
-            start = start.getNextNode()
-        return largest
-
-    # method returns minimum element of Linked list
-    def Min(self):
-        start = self.head
-        smallest = start.getData()
-        while start:
-            if smallest > start.getData():
-                smallest = start.getData()
-            start = start.getNextNode()
-        return smallest
-
-    # method pushes element to the Linked List
-    def push(self, data):
-        self.addToEnd(data)
-        return True
-
-    # method removes and returns the last element from the Linked List
-    def pop(self):
-        start = self.head
-        previous = None
-
-        while start.getNextNode():
-            previous = start
-            start = start.getNextNode()
-
-        if previous is None:
-            self.head = None
-        else:
-            previous.setNext(None)
-            data = start.getData()
-            del start
-            return data
-
-    # method returns the element at given position
-
-    def atIndex(self, position):
-        start = self.head
-        position = int(position)
-        pos = 1
-        while pos != position:
-            start = start.getNextNode()
-            pos += 1
-
-        data = start.getData()
-        return data
-
-    # method returns a copy of the current Linked List
-    def copy(self):
-        temp = LinkedList()
-        start = self.head
-
-        temp.addToStart(start.getData())
-        start = start.getNextNode()
-
-        while start:
-            temp.addToEnd(start.getData())
-            start = start.getNextNode()
-
-        return temp
-
-    # method to clear LinkedList
-    def clear(self):
-        self.head = None
-        return True
-
-    # method returns and removes element at recieved position
-    def removePosition(self, position):
-        data = self.atIndex(position)
-        self.remove(data)
-        return data
-
-    # method returns string of elements of Linked list
-    # the Elements are seperated by seperator if passed else all elements are appended
-    def toString(self, seperator=""):
-        start = self.head
-        finalString = ""
-        while start:
-            tempString = start.getData()
-            finalString += str(tempString)
-            start = start.getNextNode()
-
-            # if next node exists only the append seperator
-            if start:
-                finalString += seperator
-
-        return finalString
-
-    # method returns count of Element recieved
-    def count(self, element):
-        start = self.head
-        count1 = 0
-        while start:
-            if start.getData() == element:
-                count1 += 1
-            start = start.getNextNode()
-        return count1
-
-    # method returns builtin List of python consisting of Elements of LinkedList
-    def toList(self):
-        start = self.head
-        tempList = []
-        while start:
-            tempElement = start.getData()
-            tempList.append(tempElement)
-            start = start.getNextNode()
-        return tempList
-
-        # method returns builtin List of python consisting of Elements of LinkedList
-
-    # method returns builtin Set of python consisting of Elements of LinkedList
-    def toSet(self):
-        start = self.head
-        tempSet = set()
-        while start:
-            tempElement = start.getData()
-            if tempElement not in tempSet:
-                tempSet.add(tempElement)
-            start = start.getNextNode()
-        return tempSet
-
-    # method reverses the LinkedList
     def reverse(self):
-        start = self.head
-        tempNode = None
-        previousNode = None
-
-        while start:
-            tempNode = start.getNextNode()
-            start.setNext(previousNode)
-            previousNode = start
-            start = tempNode
-
-        self.head = previousNode
-        return True
-
-    # method that sorts LinkedList
-    def sort(self):
-        start = self.head
-        beginNode = start
-        while beginNode:
-            tempNode = beginNode
-            tempNode2 = beginNode
-            smallest = beginNode.getData()
-            while tempNode:
-                if smallest > tempNode.getData():
-                    smallest = tempNode.getData()
-                    tempNode2 = tempNode
-                tempNode = tempNode.getNextNode()
-
-            # swap data of beginNode and tempNode2
-            temp = beginNode.getData()
-            beginNode.updateData(tempNode2.getData())
-            tempNode2.updateData(temp)
-
-            beginNode = beginNode.getNextNode()
-
-    # method returns new instance of the sorted LinkedList without changing original LinkedList
-    def sorted(self):
-        start = self.head
-        tempList = self.copy()
-        tempList.sort()
-        return tempList
+        """
+        Reverse the list in-place.
+        Takes O(n) time.
+        """
+        curr = self.head
+        prev_node = None
+        next_node = None
+        while curr:
+            next_node = curr.next
+            curr.next = prev_node
+            prev_node = curr
+            curr = next_node
+        self.head = prev_node
 
 
-# node class
+if __name__ == "__main__":
+    myList = SinglyLinkedList()
+    myList.append(5)
+    print(myList)
