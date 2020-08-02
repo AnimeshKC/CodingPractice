dc3e22eaee1c7cb388d824c355e4f02075f02d4a
AnimeshKC <animesh@ualberta.ca>
2020-08-02 09:48:26 -0600

    create offset button component

---
+++ WebDevPractice/React/simple-counter/src/components/OffsetButton.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function OffsetButton(props) {
+  const { offset, offsetFunction } = props
+  let buttonSign = ""
+  if (offset > 0) buttonSign = "+"
+  else if (offset < 0) buttonSign = "-"
+
+  function handleClick() {
+    offsetFunction(offset)
+  }
+  const buttonString = `${buttonSign} offset`
+  return <button onClick={handleClick}>{buttonString}</button>
+}
