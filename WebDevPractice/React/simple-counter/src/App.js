c783e45fb039abfff1924e752d6a4ea9b05d1c3d
AnimeshKC <animesh@ualberta.ca>
2020-08-02 10:00:09 -0600

    adding core button functionality

--- WebDevPractice/React/simple-counter/src/App.js
+++ WebDevPractice/React/simple-counter/src/App.js
@@ -1,9 +1,30 @@
-import React from "react"
-import logo from "./logo.svg"
+import React, { useState } from "react"
 import "./App.css"
-
+import OffsetButton from "./components/OffsetButton"
 function App() {
-  return <div className="App">Nothing for now</div>
+  const [count, setCount] = useState(0)
+
+  function handleOffset(offsetValue) {
+    setCount(count + offsetValue)
+  }
+  /*
+  have state for count and a set count function
+
+  have an increment function that will be passed to the respective buttons
+
+
+  */
+  return (
+    <div className="App">
+      <div className="buttonContainer">
+        <OffsetButton offset={1} offsetFunction={handleOffset}></OffsetButton>
+        <OffsetButton offset={-1} offsetFunction={handleOffset}></OffsetButton>
+        <OffsetButton offset={10} offsetFunction={handleOffset}></OffsetButton>
+        <OffsetButton offset={100} offsetFunction={handleOffset}></OffsetButton>
+      </div>
+      <div>{count}</div>
+    </div>
+  )
 }
 
 export default App
