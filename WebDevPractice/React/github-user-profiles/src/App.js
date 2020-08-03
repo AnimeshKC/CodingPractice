6826fb112684072f652f25a09f2a9cb9cf4bd791
AnimeshKC <animesh@ualberta.ca>
2020-08-03 10:51:21 -0600

    created function to add github cards

--- WebDevPractice/React/github-user-profiles/src/App.js
+++ WebDevPractice/React/github-user-profiles/src/App.js
@@ -1,8 +1,19 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css"
-
+import Form from "./components/Form"
 function App() {
-  return <div className="App">Nothing for now</div>
+  const [cards, setCards] = useState([])
+  function addCard(newCard) {
+    setCards([...cards, newCard])
+  }
+  return (
+    <div>
+      <h1 className="pt-10 text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+        Search a GitHub User
+      </h1>
+      <Form onSubmit={addCard} />
+    </div>
+  )
 }
 
 export default App
