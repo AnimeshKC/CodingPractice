db3e7b4f25aa4598c3d47b1044ef1ea76e9112b6
AnimeshKC <animesh@ualberta.ca>
2020-11-21 17:35:40 -0700

    Applying MVC Route Attributes

---
+++ c#Practice/ASP.NET/PassDataFromControllerToView/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PassDataFromControllerToView.Models;
+
+namespace PassDataFromControllerToView.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.User = "Default";
+            ViewBag.Preference = "None";
+            return View();
+        }
+        public ActionResult GetEmployee()
+        {
+            var employees = new List<Employee>() {new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "John Smith"
+            },new Employee()
+            {
+                EmployeeId = 2,
+                EmployeeName = "Jane Smith"
+            }
+        };
+
+            ViewBag.Employees = employees;
+            return View();
+        }
+    }
+}
---
+++ c#Practice/ASP.NET/PassDataFromControllerToView/Controllers/ManagerController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassDataFromControllerToView.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: Manager
+        public ActionResult Index()
+        {
+            return Content("Just a Test Return");
+        }
+        [Route("Manager/PassValue/{value}")]
+        public ActionResult PassValue(int value)
+        {
+            return Content("The Value is " + value);
+        }
+    }
+}
